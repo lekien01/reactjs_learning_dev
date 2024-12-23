39a78bfd0598dda0eeec179f03f6fb7de585bc5c
Kien Dinh <kiendinh@Kiens-Mac-mini.local>
2024-12-23 08:38:12 -0700

    mmes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './tutorial/04-project-structure/starter/Navbar'
 import { Home, About } from './tutorial/04-project-structure/starter/Pages'
 import List from './tutorial/05-leverage-javascript/starter/List'
 import Final from './tutorial/06-forms/starter/01-controlled-inputs'
+import Starter from './tutorial/06-forms/starter/01-controlled-inputs'
 function App() {
   return (
     <div className="container">
@@ -12,7 +13,7 @@ function App() {
       <About />
       <Example />
       <List />
-
+      <Starter />
       {/* <Final /> */}
       {/* <h2>Advanced React</h2> */}
     </div>
--- src/tutorial/06-forms/starter/01-controlled-inputs.jsx
+++ src/tutorial/06-forms/starter/01-controlled-inputs.jsx
@@ -1,18 +1,33 @@
 import { useState } from 'react'
+import { data } from '../../../data'
 
 const ControlledInputs = () => {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
+  const [users, setUsers] = useState(data)
 
   const handleSubmit = (e) => {
     e.preventDefault()
     // do something
+    console.log(e.target.value)
     console.log(name, email)
+    if (!name || !email) return
+    const fakeID = Date.now()
+    // console.log(fakeID)
+    const newUser = { id: fakeID, name }
+    const updateUsers = [...users, newUser]
+    setUsers(updateUsers)
+    setName('')
+    setEmail('')
+  }
+  const removeUser = (id) => {
+    const updateUsers = users.filter((person) => person.id !== id)
+    setUsers(updateUsers)
   }
   return (
     <div>
       <form className="form" onSubmit={handleSubmit}>
-        <h4>ControlledInputs</h4>
+        <h4>control inputs</h4>
         <div className="form-row">
           <label htmlFor="name" className="form-label">
             name
@@ -43,6 +58,21 @@ const ControlledInputs = () => {
           Submit
         </button>
       </form>
+      <h4>users</h4>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <h4>{user.name}</h4>
+            <button
+              type="button"
+              className="btn "
+              onClick={() => removeUser(user.id)}
+            >
+              Remove
+            </button>
+          </div>
+        )
+      })}
     </div>
   )
 }
